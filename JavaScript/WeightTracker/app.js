c8f73ee08a9c61be90b10cec772497b9ca2237fd
Davide Tentori <dtentori@softeam-tsa.it>
2015-02-13 15:49:17 +0100

    first commit

---
+++ JavaScript/WeightTracker/app.js
@@ -0,0 +1,27 @@
+﻿
+//MODULE
+var wTrackerApp = angular.module('wTrackerApp', ['ngRoute', 'ngResource']);
+
+//ROUTES
+wTrackerApp.config(function ($routeProvider) {
+
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/charts.html',
+        controller: 'chartsController'
+    })
+
+    .when('/', {
+        templateUrl: 'pages/dataEntry.html',
+        controller: 'dataEntryController'
+    })
+});
+
+wTrackerApp.controller('chartsController', ['$scope'], function ($scope) {
+
+})
+
+wTrackerApp.controller('dataEntryController', ['$scope'], function ($scope) {
+
+})
