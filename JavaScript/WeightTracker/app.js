9add4b43b4bb9410e8a7b6277ab4326b68b49f4f
Davide Tentori <dtentori@softeam-tsa.it>
2015-02-13 17:14:41 +0100

    ng prj setup

--- JavaScript/WeightTracker/app.js
+++ JavaScript/WeightTracker/app.js
@@ -1,27 +1,40 @@
-﻿
-//MODULE
+﻿//MODULE
 var wTrackerApp = angular.module('wTrackerApp', ['ngRoute', 'ngResource']);
 
 //ROUTES
 wTrackerApp.config(function ($routeProvider) {
-
+    
     $routeProvider
-
+    
     .when('/', {
-        templateUrl: 'pages/charts.html',
-        controller: 'chartsController'
+            templateUrl: 'pages/dataEntry.html',
+            controller: 'dataEntryController'
     })
-
-    .when('/', {
-        templateUrl: 'pages/dataEntry.html',
-        controller: 'dataEntryController'
-    })
+    
+    .when('/charts', {
+            templateUrl: 'pages/charts.html',
+            controller: 'chartsController'
+    });
 });
 
-wTrackerApp.controller('chartsController', ['$scope'], function ($scope) {
+wTrackerApp.controller('chartsController', ['$scope', 'curWeigthService', function ($scope, curWeigthService) {
 
-})
+    $scope.curWeigth = curWeigthService.curWeigth;
+}
+]);
 
-wTrackerApp.controller('dataEntryController', ['$scope'], function ($scope) {
+wTrackerApp.controller('dataEntryController', ['$scope', 'curWeigthService', function ($scope, curWeigthService) {
 
-})
+  $scope.curWeigth = curWeigthService.curWeigth;
+    
+  $scope.$watch('curWeigth', function() {
+            curWeigthService.curWeigth = $scope.curWeigth;
+  });
+}
+]);
+
+
+//SERVICES
+wTrackerApp.service('curWeigthService', function() {
+    this.curWeigth = 0;
+});
