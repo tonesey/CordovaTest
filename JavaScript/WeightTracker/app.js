95628b74d89f56a16b3fc3c7d199ead136e5c4ce
Davide Tentori <dtentori@softeam-tsa.it>
2015-02-16 17:56:36 +0100

    tests

--- JavaScript/WeightTracker/app.js
+++ JavaScript/WeightTracker/app.js
@@ -1,5 +1,5 @@
 ﻿//MODULE
-var wTrackerApp = angular.module('wTrackerApp', ['ngRoute', 'ngResource']);
+var wTrackerApp = angular.module('wTrackerApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
 
 //ROUTES
 wTrackerApp.config(function ($routeProvider) {
@@ -10,29 +10,57 @@ wTrackerApp.config(function ($routeProvider) {
             templateUrl: 'pages/dataEntry.html',
             controller: 'dataEntryController'
     })
-    
     .when('/charts', {
             templateUrl: 'pages/charts.html',
             controller: 'chartsController'
+    })
+    .when('/info', {
+        templateUrl: 'pages/info.html',
+        controller: 'infoController'
     });
 });
 
-wTrackerApp.controller('chartsController', ['$scope', 'curWeigthService', function ($scope, curWeigthService) {
 
+
+wTrackerApp.controller('chartsController', ['$scope', 'curWeigthService', function ($scope, curWeigthService) {
     $scope.curWeigth = curWeigthService.curWeigth;
 }
 ]);
 
 wTrackerApp.controller('dataEntryController', ['$scope', 'curWeigthService', function ($scope, curWeigthService) {
-
   $scope.curWeigth = curWeigthService.curWeigth;
-    
   $scope.$watch('curWeigth', function() {
             curWeigthService.curWeigth = $scope.curWeigth;
   });
 }
 ]);
 
+wTrackerApp.controller('infoController', ['$scope', function ($scope) {
+}
+]);
+
+wTrackerApp.controller('ButtonsCtrl', function ($scope) {
+    $scope.singleModel = 1;
+    $scope.radioModel = 'opt1';
+    $scope.checkModel = {
+        opt1: false,
+        opt2: true,
+        opt3: false
+    };
+});
+
+wTrackerApp.controller('TabsCtrl', function ($scope, $window) {
+    $scope.tabs = [
+      { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
+      { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
+    ];
+
+    $scope.alertMe = function () {
+        setTimeout(function () {
+            $window.alert('You\'ve selected the alert tab!');
+        });
+    };
+});
 
 //SERVICES
 wTrackerApp.service('curWeigthService', function() {
